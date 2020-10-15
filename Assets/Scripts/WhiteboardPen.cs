182a6e94134f4c1c6ecd18ec348f0bd17c159e00
andrasimion99 <61377543+andrasimion99@users.noreply.github.com>
2020-10-15 08:37:32 +0300

    first commit

---
+++ Assets/Scripts/WhiteboardPen.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class WhiteboardPen : VRTK_InteractableObject
+{
+    public Whiteboard whiteboard;
+    private RaycastHit touch;
+    private bool lastTouch;
+    private Quaternion lastAngle;
+    public Color color;
+
+    void Start()
+    {
+        //this.whiteboard = whiteboard.GetComponent<Whiteboard>();
+    }
+
+    void Update()
+    {
+        float height = transform.localScale.y;
+        Vector3 tip = transform.position;
+
+        if(lastTouch)
+        {
+            height = 1.1f;
+        }
+
+        if(Physics.Raycast(tip, transform.up, out touch, height))
+        {
+            if (!(touch.collider.tag == "Whiteboard"))
+                return;
+            this.whiteboard = touch.collider.GetComponent<Whiteboard>();
+
+            this.whiteboard.SetColor(color);
+            this.whiteboard.SetTouchPosition(touch.textureCoord.x, touch.textureCoord.y);
+            this.whiteboard.ToggleTouch(true);
+
+            if(!lastTouch)
+            {
+                lastTouch = true;
+                lastAngle = transform.rotation;
+            }
+        } else
+        {
+            this.whiteboard.ToggleTouch(false);
+            lastTouch = false;
+        }
+
+        if(lastTouch)
+        {
+            transform.rotation = lastAngle;
+        }
+    }
+}
